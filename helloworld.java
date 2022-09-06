67e7d5d4189a6982ee06272b6774ab158d014587
vinu dasari <vinay.greatlearning06@gmail.com>
2022-09-06 23:22:09 +0530

    this is the first commit

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+class HelloWorld
+{
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        System.out.println("Hello, World");
+System.out.println("hi this is vinay siddarth");
+System.out.println("hi this is li");
+
+    }
+}
